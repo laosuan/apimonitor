7468d34f320aac96b910dc8e33031bfa0ada60f0
mike <wxliuxuan@gfpartner.com.cn>
2022-04-21 13:43:34 +0800

    update

--- build.sh
+++
@@ -1,3 +0,0 @@
-mvn clean install
-docker build -t="apimonitor:20220421" .
-docker run -d --name apimonitor -p 3003:8080 --restart always apimonitor:20220421
---
+++ scp_mike.sh
@@ -0,0 +1,2 @@
+mvn clean install
+scp  ./target/apimonitor-0.0.1-SNAPSHOT.jar mike:/home/mike/github/java/apimonitor/target
